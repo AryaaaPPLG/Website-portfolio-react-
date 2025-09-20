765272a545263e8d691f39517c42da6a381cfd57
AryaPPLG <aryapranajaya02@gmail.com>
2025-09-20 14:26:08 +0700

    fix error

--- src/assets/components/CertificateList.jsx
+++ src/assets/components/CertificateList.jsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
 import { Download, X } from "lucide-react";
-import "react-pdf/dist/Page/AnnotationLayer.css";
-import "react-pdf/dist/Page/TextLayer.css";
 
-// Load worker
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const certificates = [
   {
